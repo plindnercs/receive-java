ccc83660a2f82ee5dc7b87a07c9e5d7ecda69d8d
Peter Lindner <peter.lindner2@stud.sbg.ac.at>
2023-06-23 14:09:55 +0200

    implemented sliding window functionality

--- src/main/java/edu/plus/cs/Receiver.java
+++ src/main/java/edu/plus/cs/Receiver.java
@@ -6,13 +6,8 @@ import edu.plus.cs.util.OperatingMode;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Stack;
+import java.net.*;
+import java.util.*;
 
 public class Receiver {
     private DatagramSocket socket;
@@ -25,6 +20,9 @@ public class Receiver {
     private int windowSize;
     private List<Packet> windowBuffer;
     private Stack<Integer> missingPackets;
+    private int expectedPacket = 0;
+    private boolean retryMode = false;
+    private int windowStart = 0;
 
     public Receiver(short transmissionId, int port, File dropOffFolder, InetAddress ackIp, int ackPort,
                     OperatingMode operatingMode, int windowSize) {
@@ -52,7 +50,61 @@ public class Receiver {
             DatagramPacket udpPacket = new DatagramPacket(buffer, buffer.length);
 
             try {
-                socket.receive(udpPacket);
+                try {
+                    socket.receive(udpPacket);
+                } catch (SocketTimeoutException ex) {
+                    if (operatingMode == OperatingMode.SLIDING_WINDOW) {
+                        // transmission of window is complete, now check for missing packets
+                        // otherwise continue bulk processing
+                        if (missingPackets.isEmpty() && !windowBuffer.isEmpty()) {
+                            retryMode = false;
+                            windowBuffer.sort(Comparator.comparing(Packet::getSequenceNumber));
+                            Packet lastPacket = windowBuffer.get(windowBuffer.size() - 1);
+                            sendAcknowledgementPacket(transmissionId, lastPacket.getSequenceNumber());
+
+                            for (Packet windowPacket : windowBuffer) {
+                                if (!digest.handlePacket(transmissionId, windowPacket)) {
+                                    System.err.println("Error while handling packet: " + windowPacket);
+                                }
+                            }
+
+                            if (windowBuffer.stream().anyMatch(packet -> packet instanceof FinalizePacket)) {
+                                socket.setSoTimeout(60000);
+                                expectedPacket = 0;
+                            }
+
+                            windowBuffer.clear();
+
+                            windowStart += windowSize;
+                        } else {
+                            // first we check if the window is not complete
+                            if (missingPackets.isEmpty() && windowBuffer.size() != windowSize) {
+                                windowBuffer.sort(Comparator.comparing(Packet::getSequenceNumber));
+
+                                // add missing sequence numbers such that the window is full
+                                for (int i = windowStart; i < windowStart + windowSize; i++) {
+                                    if (missingPackets.search(i) == -1) {
+                                        missingPackets.push(i);
+                                    }
+                                }
+                            }
+
+                            // we retry a lost packet
+                            expectedPacket = missingPackets.pop();
+
+                            System.out.println("Requesting lost packet: " + expectedPacket);
+
+                            sendAcknowledgementPacket(transmissionId, expectedPacket);
+                            sendAcknowledgementPacket(transmissionId, expectedPacket);
+
+                            retryMode = true;
+                        }
+
+                        continue;
+                    } else {
+                        System.err.println("Socket timed out!");
+                    }
+                }
 
                 Packet packet;
                 int maxSequenceNumber;
@@ -72,6 +124,12 @@ public class Receiver {
                     transmissions.put(transmissionId, maxSequenceNumber);
 
                     System.out.println("Received initialization packet at: " + System.currentTimeMillis());
+
+                    // set timeout for socket in sliding window mode, since we have to
+                    // recognize once the transmitter is finished with sending a window
+                    if (operatingMode == OperatingMode.SLIDING_WINDOW) {
+                        socket.setSoTimeout(50);
+                    }
                 } else {
                     // no initialization packet seen before for this transmissionId -> abort transmission
                     if (!transmissions.containsKey(transmissionId)) {
@@ -89,15 +147,33 @@ public class Receiver {
                     }
                 }
 
+                windowBuffer.add(packet);
+
+                if (packet.getSequenceNumber() != expectedPacket) {
+                    System.out.println("Expected packet: " + expectedPacket + " but got Packet: " +
+                            packet.getSequenceNumber());
+                    missingPackets.push(expectedPacket);
+
+                    expectedPacket++;
+                }
+
                 // printPacket(packet);
 
-                if (digest.handlePacket(transmissionId, packet)) {
-                    // only send acknowledgement if the operating mode requires to
-                    if (operatingMode == OperatingMode.STOP_WAIT) {
-                        sendAcknowledgementPacket(transmissionId, packet.getSequenceNumber());
+                // process single packet
+                if (operatingMode != OperatingMode.SLIDING_WINDOW) {
+                    if (digest.handlePacket(transmissionId, packet)) {
+                        // only send acknowledgement if the operating mode requires to
+                        if (operatingMode == OperatingMode.STOP_WAIT) {
+                            sendAcknowledgementPacket(transmissionId, packet.getSequenceNumber());
+                        }
+                    } else {
+                        System.err.println("Error while handling packet: " + packet);
                     }
                 } else {
-                    System.err.println("Error while handling packet: " + packet);
+                    if (!retryMode) {
+                        // we are still in correct order
+                        expectedPacket++;
+                    }
                 }
             } catch (Exception e) {
                 System.err.println(e);
